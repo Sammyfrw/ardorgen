f38e12904374f050c6dc8aaa342f9038570d4ae8
Sammyfrw <sammyfrw@gmail.com>
2015-11-17 11:29:13 -0500

    Clean user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,4 @@ class User < ActiveRecord::Base
   def role_is?(minimum_role)
     ROLE.index(self.role) >= ROLE.index(minimum_role)
   end
-
-  # possible_roles = [user, veteran, moderator, admin]
-  # validates :name, presence: true, uniqueness: true
-  # validates :role, presence: true
-
-  # def role_is?(minimum_role)
-  #   possible_roles.index(self.role) >= possible_roles.index(minimum_role)
-  # end
-
 end
